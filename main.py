2092b4d0720152fa957383f45d3a538703f4f968
garpil124 <kurniawansyahrifky122@gmail.com>
2026-02-20 04:05:56 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from telegram.ext import (
 from datetime import datetime
 
 OWNER_ID = 8209644174
-LOG_GROUP_ID = -1003724444499  # ganti ini dengan ID grup Telegram kamu
+LOG_GROUP_ID = -1003724444499  
 produk_file = "produk.json"
 saldo_file = "saldo.json"
 deposit_file = "pending_deposit.json"
@@ -719,6 +719,7 @@ def main(): # Made With love by @govtrashit A.K.A RzkyO
 
 if __name__ == "__main__":
     main()
+
 
 
 
