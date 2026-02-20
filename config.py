7e272ad1c84e0cf4717183d62f8a790021e8834c
elzahraku <malikelza9@gmail.com>
2026-02-20 13:34:05 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+OWNER_ID = 8209644174
+LOG_GROUP_ID = -1003828328341
+ADMIN_USERNAME = "@Brsik23"
+
+# File Paths
+PRODUK_FILE = "produk.json"
+SALDO_FILE = "saldo.json"
+DEPOSIT_FILE = "pending_deposit.json"
+RIWAYAT_FILE = "riwayat.json"
+STATISTIK_FILE = "statistik.json"
+
